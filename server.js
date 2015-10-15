9c134712c0270d67040217377ce8055a9c8af9b8
Campbell Allen <campbell.allen@gmail.com>
2015-10-15 15:26:27 +0100

    add logging and more configuration opts

--- server.js
+++ server.js
@@ -4,7 +4,8 @@
 var express = require('express')
 	, fs = require('fs')
 	, Img = require('./lib/image.js')
-	, gm = require('gm');
+	, gm = require('gm')
+	, morgan = require('morgan')
 	;
 
 /**
@@ -14,11 +15,20 @@ var img_path = __dirname + '/img/';
 if (!fs.existsSync(img_path)) {
 	fs.mkdirSync(img_path);
 }
-
+var resize_message = 'Specify an url \'u\', width \'w\' and height \'h\'';
+var crop_message = 'Specify an url \'u\', width \'w\', height \'h\', x-offset \'x\' and y-offset \'y\'';
 /**
  * Express server.
  */
-var app = express();
+var app = express()
+  .use(morgan('dev'))
+	.set('static_server', process.env.NODE_STATIC_SERVER || "http://zooniverse-static.s3-website-us-east-1.amazonaws.com/")
+  .set('port', process.env.NODE_PORT || 8080)
+  .set('env', process.env.NODE_ENV || "development")
+  .set('debug', process.env.NODE_DEBUG || false);
+
+// server to find the images on
+var static_host = app.settings.static_server;
 
 
 /**
@@ -30,16 +40,15 @@ app.get('/resize', function(req, res, next){
 		, h = req.query.h // height
 		, o = req.query.o // opts, support http://www.graphicsmagick.org/GraphicsMagick.html#details-resize
 		;
-	if (!u || !w || !h) res.send(400, {error: 'Specify an url \'u\', width \'w\' and height \'h\''});
+	if (!u || !w || !h) res.send(400, {error: resize_message});
 
-	u = "http://zooniverse-static.s3-website-us-east-1.amazonaws.com/" + u
-	var img = new Img(u,img_path).load(function(err,im) {
-		if (err) res.send(500, { error: err });
+	var img = new Img(static_host + u,img_path).load(function(err,im) {
+		if (err) res.status(500).send({ error: err })
 		else {
 			gm(im.destHashed)
 			  .resize(w, h, o)
 		    .stream(function streamOut (err, stdout, stderr) {
-					if (err) res.send(500, { error: err });
+					if (err) res.status(500).send({ error: err })
 		      stdout.pipe(res); //pipe to response
 		      stdout.on('error', next);
 		    });
@@ -52,14 +61,13 @@ app.get('/resize_crop', function(req, res, next){
 		, w = req.query.w // width
 		, h = req.query.h // height
 		;
-	if (!u || !w || !h) res.send(400, {error: 'Specify an url \'u\', width \'w\' and height \'h\''});
+	if (!u || !w || !h) res.send(400, {error: resize_message});
 
-	u = "http://zooniverse-static.s3-website-us-east-1.amazonaws.com/" + u
-	var img = new Img(u,img_path).load(function(err,im) {
-		if (err) res.send(500, { error: err });
+	var img = new Img(static_host + u,img_path).load(function(err,im) {
+		if (err) res.status(500).send({ error: err })
 		else {
 			im.resize(w,h,function(err,im) {
-				if (err) res.send(500, { error: err });
+				if (err) res.status(500).send({ error: err })
 				else {
 					res.writeHead(200, {"Content-Type": "image/" + im.extension});
       		res.end(im.resized, "binary");
@@ -76,23 +84,21 @@ app.get('/crop', function(req, res, next){
 		, x = req.query.x // x-offset
 		, y = req.query.y // y-offset
 		;
-	if (!u || !w || !h || !x || !y) res.send(400, {error: 'Specify an url \'u\', width \'w\', height \'h\' x-offset \'x\' and y-offset \'y\''});
+	if (!u || !w || !h || !x || !y) res.send(400, {error: crop_message});
 
-	u = "http://zooniverse-static.s3-website-us-east-1.amazonaws.com/" + u
-	var img = new Img(u,img_path).load(function(err,im) {
-		if (err) res.send(500, { error: err });
+	var img = new Img(static_host + u,img_path).load(function(err,im) {
+		if (err) res.status(500).send({ error: err })
 		else {
-			console.log(im)
+			if (app.get("debug")) {
+				console.log("\nCrop image:");
+				console.log(im);
+			}
 			res.writeHead(200, {"Content-Type": "image/" + im.extension});
 			gm(im.destHashed)
 		    .crop(w, h, x, y)
 		    .stream(function streamOut (err, stdout, stderr) {
-					if (err) res.send(500, { error: err });
-		      // if (err) {
-					//   stdout.on('error', next);
-					// 	return next(err);
-					// }
-		      stdout.pipe(res); //pipe to response
+					if (err) res.status(500).send({ error: err })
+		      stdout.pipe(res);
 		      stdout.on('error', next);
 		    });
 		}
@@ -103,6 +109,6 @@ app.get('/crop', function(req, res, next){
 /**
  * Start server.
  */
-app.listen(3000, function() {
+app.listen(app.get("port"), function() {
 	console.log("Express server listening on port %d in %s mode", app.settings.port || 3000, app.settings.env);
 });
